859c1c56550f7fe3269269f5b91b7f967e8d0d85
Tejashwinim10 <tejashwiniat123@gmail.com>
2025-03-25 09:56:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import name from './name';
+function Navbar(){
+    return(
+     <name/>
+    );
+}
+export default name
---
+++ src/name.jsx
@@ -0,0 +1,8 @@
+function name(){
+  return(
+<nav className="card1" >
+  <h2 className="card"> Tejashwini !!</h2>
+</nav>
+  );
+}
+export default name
